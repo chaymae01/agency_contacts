fb7afc9bfa491bc17f9379853f75060876fe9066
elfahssichaymae <chaymaeelfahssi031@gmail.com>
2025-11-30 18:04:48 +0100

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -4,6 +4,6 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico).*)',
   ],
 };
