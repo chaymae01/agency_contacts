9775f7ca8813218e592405f8742047fc30d1c57a
chaymae01 <chaymaeelfahssi031@gmail.com>
2025-11-30 16:10:28 +0100

    Ajout publishabl

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider 
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY || ''}
       localization={frFR}
     >
       <html lang="fr">
